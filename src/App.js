801696c3304d04d11b1f5c81f6969b045f952c79
Vinicius Pietscher <vini.914@gmail.com>
2022-04-12 16:20:40 -0400

    Callback Handlers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
+const App = () => {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ]
+  const jsLibraries = [
+    {
+      title: "Google Polymer",
+      url: "https://en.wikipedia.org/wiki/Google_Polymer",
+      author: "Google",
+      num_comments: 3,
+      points: 4,
+      objectID: 2,
+    },
+    {
+      title: "Angular",
+      url: "https://angular.io",
+      author: "Google",
+      num_comments: 2,
+      points: 5,
+      objectID: 3,
+    },
+  ]
+
+  const handleSearch = (event) => {
+    console.log(event.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>My Hacker Stories</h1>
+      <Search onSearch={handleSearch} />
+      <hr />
+      <List list={stories} />
+      <List list={jsLibraries} />
+    </>
+  )
 }
 
-export default App;
+const Search = (props) => {
+  const [searchTerm, setSearchTerm] = React.useState("")
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value)
+    props.onSearch(event)
+  }
+
+  return (
+    <>
+      <label htmlFor='search'>Search: </label>
+      <input id='search' type='text' onChange={handleChange} />
+
+      <p>
+        Searching for <strong>{searchTerm}</strong>
+      </p>
+    </>
+  )
+}
+
+const List = (props) => (
+  <ul>
+    {props.list.map((item) => (
+      <Item key={item.objectID} item={item} />
+    ))}
+  </ul>
+)
+const Item = (props) => (
+  <li>
+    <span>
+      <a href={props.item.url}>{props.item.title}</a>
+    </span>
+    <span>{props.item.author}</span>
+    <span>{props.item.num_comments}</span>
+    <span>{props.item.points}</span>
+  </li>
+)
+export default App
