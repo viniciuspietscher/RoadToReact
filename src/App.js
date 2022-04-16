24dad21c5be0e8f825f1751648323e8f8af208f9
Vinicius Pietscher <vini.914@gmail.com>
2022-04-16 14:50:51 -0400

    props handling

--- src/App.js
+++ src/App.js
@@ -55,35 +55,30 @@ const App = () => {
   )
 }
 
-const Search = (props) => {
+const Search = ({ search, onSearch }) => {
   return (
     <>
       <label htmlFor='search'>Search: </label>
-      <input
-        id='search'
-        type='text'
-        value={props.search}
-        onChange={props.onSearch}
-      />
+      <input id='search' type='text' value={search} onChange={onSearch} />
     </>
   )
 }
 
-const List = (props) => (
+const List = ({ list }) => (
   <ul>
-    {props.list.map((item) => (
-      <Item key={item.objectID} item={item} />
+    {list.map(({ objectID, ...item }) => (
+      <Item key={objectID} {...item} />
     ))}
   </ul>
 )
-const Item = (props) => (
+const Item = ({ url, title, author, num_comments, points }) => (
   <li>
     <span>
-      <a href={props.item.url}>{props.item.title} </a>
+      <a href={url}>{title} </a>
     </span>
-    <span>{props.item.author} </span>
-    <span>{props.item.num_comments} </span>
-    <span>{props.item.points}</span>
+    <span>{author} </span>
+    <span>{num_comments} </span>
+    <span>{points}</span>
   </li>
 )
 export default App
