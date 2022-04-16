e9cad587b6bbe622cc7382b0ce665880a9a7ee2d
Vinicius Pietscher <vini.914@gmail.com>
2022-04-16 14:01:49 -0400

    lift state, controlled components

--- src/App.js
+++ src/App.js
@@ -18,56 +18,53 @@ const App = () => {
       points: 5,
       objectID: 1,
     },
-  ]
-  const jsLibraries = [
     {
-      title: "Google Polymer",
-      url: "https://en.wikipedia.org/wiki/Google_Polymer",
-      author: "Google",
-      num_comments: 3,
-      points: 4,
+      title: "Vue",
+      url: "https://vuejs.org/",
+      author: "Evan You",
+      num_comments: 2,
+      points: 5,
       objectID: 2,
     },
     {
       title: "Angular",
-      url: "https://angular.io",
+      url: "https://angular.io/",
       author: "Google",
       num_comments: 2,
       points: 5,
       objectID: 3,
     },
   ]
+  const [searchTerm, setSearchTerm] = React.useState("React")
 
   const handleSearch = (event) => {
-    console.log(event.target.value)
+    setSearchTerm(event.target.value)
   }
+
+  const searchedStories = stories.filter((story) =>
+    story.title.toLowerCase().includes(searchTerm.toLocaleLowerCase())
+  )
 
   return (
     <>
       <h1>My Hacker Stories</h1>
-      <Search onSearch={handleSearch} />
+      <Search search={searchTerm} onSearch={handleSearch} />
       <hr />
-      <List list={stories} />
-      <List list={jsLibraries} />
+      <List list={searchedStories} />
     </>
   )
 }
 
 const Search = (props) => {
-  const [searchTerm, setSearchTerm] = React.useState("")
-  const handleChange = (event) => {
-    setSearchTerm(event.target.value)
-    props.onSearch(event)
-  }
-
   return (
     <>
       <label htmlFor='search'>Search: </label>
-      <input id='search' type='text' onChange={handleChange} />
-
-      <p>
-        Searching for <strong>{searchTerm}</strong>
-      </p>
+      <input
+        id='search'
+        type='text'
+        value={props.search}
+        onChange={props.onSearch}
+      />
     </>
   )
 }
@@ -82,10 +79,10 @@ const List = (props) => (
 const Item = (props) => (
   <li>
     <span>
-      <a href={props.item.url}>{props.item.title}</a>
+      <a href={props.item.url}>{props.item.title} </a>
     </span>
-    <span>{props.item.author}</span>
-    <span>{props.item.num_comments}</span>
+    <span>{props.item.author} </span>
+    <span>{props.item.num_comments} </span>
     <span>{props.item.points}</span>
   </li>
 )
