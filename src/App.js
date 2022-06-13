6b36416cea9b7553f7efb15a746bfdc4086f4656
Vinicius Pietscher <vini.914@gmail.com>
2022-06-13 17:15:46 -0400

    third party libraries

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState, useEffect, useRef, useReducer, useCallback } from "react"
+import axios from "axios"
 
 const useSemiPersistentState = (key, initialState) => {
   const [value, setValue] = useState(localStorage.getItem(key) || initialState)
@@ -68,12 +69,12 @@ const App = () => {
   const handleFetchStories = useCallback(() => {
     if (!searchTerm) return
     dispatchStories({ type: "STORIES_FETCH_INIT" })
-    fetch(url)
-      .then((response) => response.json())
+    axios
+      .get(url)
       .then((result) => {
         dispatchStories({
           type: "STORIES_FETCH_SUCCESS",
-          payload: result.hits,
+          payload: result.data.hits,
         })
       })
       .catch(() => dispatchStories({ type: "STORIES_FETCH_FAILURE" }))
