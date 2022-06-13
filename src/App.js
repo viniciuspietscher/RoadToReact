5168600ddb17fef048f4f9752e2a64aa01e992ca
Vinicius Pietscher <vini.914@gmail.com>
2022-06-13 17:23:05 -0400

    async await

--- src/App.js
+++ src/App.js
@@ -66,18 +66,19 @@ const App = () => {
     setUrl(`${API_ENDPOINT}${searchTerm}`)
   }
 
-  const handleFetchStories = useCallback(() => {
+  const handleFetchStories = useCallback(async () => {
     if (!searchTerm) return
     dispatchStories({ type: "STORIES_FETCH_INIT" })
-    axios
-      .get(url)
-      .then((result) => {
-        dispatchStories({
-          type: "STORIES_FETCH_SUCCESS",
-          payload: result.data.hits,
-        })
+
+    try {
+      const result = await axios.get(url)
+      dispatchStories({
+        type: "STORIES_FETCH_SUCCESS",
+        payload: result.data.hits,
       })
-      .catch(() => dispatchStories({ type: "STORIES_FETCH_FAILURE" }))
+    } catch {
+      dispatchStories({ type: "STORIES_FETCH_FAILURE" })
+    }
   }, [url])
 
   useEffect(() => {
